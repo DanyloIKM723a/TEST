b5a7b832f407fcc4559cc4cc98beb423de3a0e01
Danylo Masiyk <163041483+DanyloIKM723a@users.noreply.github.com>
2024-04-19 20:46:51 +0300

    Add files via upload

---
+++ ЗАВДАННЯ 3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int stringLength(string str) {
+	return str.length(); // Uses the 'length' method of the 'string' class
+}
+
+int main() {
+	string inputString;
+
+	cout << "Enter a string: ";
+	getline(cin, inputString); // Reads the entire line of input into 'inputString'
+
+	int length = stringLength(inputString);
+	cout << "The length of the string is: " << length << endl;
+
+	return 0;
+}
