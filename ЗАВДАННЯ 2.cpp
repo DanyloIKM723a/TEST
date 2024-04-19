9ee96409e177cf8bae2271ad67c686e9b263d34d
Danylo Masiyk <163041483+DanyloIKM723a@users.noreply.github.com>
2024-04-19 20:46:37 +0300

    Add files via upload

---
+++ ЗАВДАННЯ 2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+int factorial(int n) {
+    if (n == 0) {
+        return 1;
+    }
+    else {
+        return n * factorial(n - 1);
+    }
+}
+
+int main() {
+    int num;
+
+    std::cout << "Enter a non-negative integer: ";
+    std::cin >> num;
+
+    if (num < 0) {
+        std::cout << "Invalid input: Factorial is not defined for negative numbers." << std::endl;
+        return 1;
+    }
+
+    int result = factorial(num);
+    std::cout << "Factorial of " << num << " is: " << result << std::endl;
+
+    return 0;
+}
